173a2fdbf5be8e3750fbda2d412612dc4b54010e
nandhakishoreramanan <nandhakishoreramanan@gmail.com>
2026-03-03 15:38:40 +0530

    Coded UC3 Print OOPS to Console

---
+++ uc3java/oopsbannerapp.java
@@ -0,0 +1,15 @@
+public class oopsbannerapp {
+    public static void main(String[] args) {
+        // O         O              P             S
+        System.out.println(String.join(" ", "   ***   ", "   ***   ", " ******  ", "  ***** "));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", " **   ** ", " **     "));
+        System.out.println(String.join(" ", "**     **", "**     **", " **    ** ","**     "));
+        System.out.println(String.join(" ", "**     **", "**     **", " **   **  "," ** "));
+        System.out.println(String.join(" ", "**     **", "**     **", " *****      ", "***"));
+        System.out.println(String.join(" ", "**     ** ","**     ** ", "**      ", "      **"));
+        System.out.println(String.join(" ", "**     **", "**     **", " **      ", "       ** "));
+        System.out.println(String.join(" ", " **   **", "  **   **", "  **      ", "      ** "));
+        System.out.println(String.join(" ", "   ***   ", "   ***   ", " **     ", "  ***** "));
+    }
+
+}
