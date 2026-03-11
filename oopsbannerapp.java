3a8c2ec2f4cebc7c0ae2155d7f67eeb3a1ea8acc
nandhakishoreramanan <nandhakishoreramanan@gmail.com>
2026-03-11 08:51:19 +0530

    UC4: Refactored using String array and enhanced for-loop

---
+++ oopsbannerapp.java
@@ -0,0 +1,64 @@
+/**
+ * OOPSBannerApp - Use Case 4
+ * Prints the word "OOPS" using String array and loop.
+ * Improves modularity and reusability.
+ *
+ * @author nandhakishore
+ * @version 4.0
+ */
+public class oopsbannerapp {
+
+    public static void main(String[] args) {
+
+        // Store banner lines inside an array
+        String[] banner = {
+
+                String.join("   ",
+                        " ***** ",
+                        " ***** ",
+                        " ******",
+                        " ******"),
+
+                String.join("   ",
+                        "*     *",
+                        "*     *",
+                        "*     *",
+                        "*      "),
+
+                String.join("   ",
+                        "*     *",
+                        "*     *",
+                        "****** ",
+                        " ***** "),
+
+                String.join("   ",
+                        "*     *",
+                        "*     *",
+                        "*      ",
+                        "      *"),
+
+                String.join("   ",
+                        "*     *",
+                        "*     *",
+                        "*      ",
+                        "*     *"),
+
+                String.join("   ",
+                        "*     *",
+                        "*     *",
+                        "*      ",
+                        "*     *"),
+
+                String.join("   ",
+                        " ***** ",
+                        " ***** ",
+                        "*      ",
+                        " ***** ")
+        };
+
+        // Print using enhanced for-loop
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
